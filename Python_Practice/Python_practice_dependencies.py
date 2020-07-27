34fa655eec1b81264f4bc9e75f1df2b63486aff1
Andreja Ho <sfandreja@gmail.com>
2020-07-26 23:49:22 -0700

    add practice-dependencies

---
+++ Python_Practice/Python_practice_dependencies.py
@@ -0,0 +1,10 @@
+#Import the datetime class from the datetime module.
+import datetime as dt
+#Use the now() attributte on the datetime class to get the present time.
+now = dt.datetime.now()
+#print the present time.
+print("The time right now is ", now)
+
+import csv
+dir(csv)
+
