c25287b9c5ea7791a9a5ae291310c488b5a07590
Andreja Ho <sfandreja@gmail.com>
2020-07-27 00:23:40 -0700

    add method open

--- PyPoll.py
+++ PyPoll.py
@@ -1,6 +1,26 @@
 #election-analysis
 #PSEUDOCODE
 
+#Importing files. csv & os(when path unknown)
+import csv
+import os
+
+#Open a .CSV file.
+#Assign a variable for the file to load and the path.
+#file_to_load = 'Resources/election_results.csv' #--THIS LINE -- a way to open the file when path is known
+file_to_load = os.path.join("Resources","election_results.csv") # a way when we do not know the path
+
+#Open the election results and read the file.
+#election_data = open(file_to_load, 'r') --THIS LINE-- is another way to open/read the file. "With" method below is another way.
+with open(file_to_load) as election_data:
+
+    #To do: perform analysis.
+    print(election_data)
+
+#Close the file.
+election_data.close()
+
+
 #Total number of votes cast
 ##Count all the votes form entire dataset {total_votes}
 
