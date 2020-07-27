a61121f0cd3a3101df9a407e56bade2c6c11229a
Andreja Ho <sfandreja@gmail.com>
2020-07-26 23:48:36 -0700

    add pseudocode to PyPoll.py

---
+++ PyPoll.py
@@ -0,0 +1,17 @@
+#election-analysis
+#PSEUDOCODE
+
+#Total number of votes cast
+##Count all the votes form entire dataset {total_votes}
+
+#A complete list of candidates who received votes
+##Get all candidate names from the dataset + condition who recieved the votes.
+
+#Total number of votes each candidate received
+##Set condition for candidate name and count their votes {candidate_name:number_of_votes}
+
+#Percentage of votes each candidate won
+## {candidate_name:number_of_votes} / {total_votes} * 100
+
+#The winner of the election based on popular vote
+##condition = the most votes: winner = candidate name.
